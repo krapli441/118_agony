4d310b2378145bb79f94f2c4448f6eb35bac7a54
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-18 10:27:38 +0900

    2

---
+++ 2.js
@@ -0,0 +1,8 @@
+// * 변수를 선언하고, 그 변수에 숫자 10을 할당하는 코드를 작성하세요.
+
+const number = 10;
+let numberTwo = 10;
+var numberThree = 10;
+
+console.log(number, numberTwo, numberThree);
+console.log(typeof number, typeof numberTwo, typeof numberThree);
