e2b3844b5132b1d53b877d1572909ffc8ff775e0
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-18 14:28:11 +0900

    15

---
+++ 11~20/15.js
@@ -0,0 +1,28 @@
+// * 자바스크립트에서 사용하는 데이터 타입들을 나열해보세요.
+
+let number = 10; // number, 숫자
+let string = "문자"; // string, 문자열
+let trueFalse = true; // boolean, 참(true) 혹은 거짓(false)
+let vacancy = null; // null, 값이 없음.
+let noIdentity = undefined; // undefined, 값이 할당되지 않았음.
+let myObject = {
+  name: "park",
+  age: "29",
+}; // 객체, 여러 속성과 메서드를 포함할 수 있는 데이터 타입.
+
+function answer() {
+  console.log(
+    "함수. 일급 객체로서, 자바스크립트에서는 함수를 데이터 타입으로 취급한다."
+  );
+}
+
+const list = [
+  "배열.",
+  "여러 개의 값을",
+  "순차적으로",
+  "저장하는",
+  "데이터",
+  "타입이다.",
+];
+
+const day = new Date(); // 날짜와 시간 정보를 나타내는 데이터 타입이다.
