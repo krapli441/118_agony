9dac27a88c5c784b8b532493d43ab29b131a3a4a
박준형 <108733795+krapli441@users.noreply.github.com>
2023-07-18 11:12:03 +0900

    8

---
+++ 8.js
@@ -0,0 +1,19 @@
+// * 자바스크립트에서 식별자로 사용될 수 있는 예를 3가지 제시하세요.
+
+// * 1. 식별자는 반드시 문자, 달러 기호($), 밑줄(_)로 시작해야 한다.
+
+let name;
+let age;
+let person;
+let $height;
+let _tall;
+
+// * 2. 첫 번째 문자 다음에는 숫자가 나올 수 있다.
+
+let p3rson;
+
+// * 3. 예약어는 식별자로 사용할 수 없다. 예약어란, 프로그래밍 언어에서 특별한 용도로 사용되는 단어를 말한다.
+
+let if; // syntax error
+let else; // syntax error
+let for; // syntax error
