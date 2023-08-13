07f21493460a8e1cba1f13d413ced0c7f0cb6e5d
krapli441 <tlfqj1208@gmail.com>
2023-08-13 14:08:28 +0900

    24.js

---
+++ 21~30/24.js
@@ -0,0 +1,11 @@
+// * x가 y보다 큰지 확인하는 조건식을 작성해보세요.
+
+function checker(x, y) {
+  if (x > y) {
+    console.log(`${x} is bigger than ${y}`);
+  } else {
+    console.log(`${x} is smaller than ${y}`);
+  }
+}
+
+checker(10, 5);
