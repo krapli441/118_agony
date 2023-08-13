80abbe556592e5683efa65720fc34bdc127cd621
krapli441 <tlfqj1208@gmail.com>
2023-08-13 15:03:20 +0900

    38

---
+++ 31~40/38.js
@@ -0,0 +1,19 @@
+// * 객체의 속성에 접근하는 두 가지 방법을 코드로 작성해보세요.
+
+// * 1.
+
+let obj = {
+  name: "John",
+  age: 30,
+};
+
+console.log(obj.name);
+
+// * 2.
+
+let obj2 = {
+  name: "James",
+  age: 30,
+};
+
+console.log(obj2["name"]);
