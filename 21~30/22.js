742cc77ad4aa9bfedfbdce40010f8f7bb5975275
krapli441 <tlfqj1208@gmail.com>
2023-08-13 14:04:59 +0900

    22.js

---
+++ 21~30/22.js
@@ -0,0 +1,22 @@
+// * +, -, *, / 연산자를 사용해 간단한 산술 연산을 하는 코드를 작성해보세요.
+
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function dis(a, b) {
+  return a / b;
+}
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(multiply(1, 2));
+console.log(dis(1, 2));
