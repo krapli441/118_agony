302e205ae1f99f35962a9d859320734af631a8c5
krapli441 <tlfqj1208@gmail.com>
2023-08-25 19:26:26 +0900

    48

---
+++ 41~50/48.js
@@ -0,0 +1,11 @@
+// * 배열의 모든 원소를 순회하며 출력하는 코드를 작성하세요.
+
+let array = [1, 2, 3, 4, 5];
+
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
+console.log(printArray(array)); // 1 2 3 4 5
