6a01f351d42a87aa06c9aaf4e6da2253cb1b6c68
krapli441 <tlfqj1208@gmail.com>
2023-08-29 20:26:00 +0900

    54

--- 51~60/54.js
+++ 51~60/54.js
@@ -1,7 +1,8 @@
 // * 콜백 함수를 사용하는 코드를 작성해보세요.
 
-function callbackexample(callback) {
-  callback(console.log("콜백함수 호출"));
+function callbackAdd(a, b, callback) {
+  let result = a + b;
+  callback(result);
 }
 
-console.log(callbackexample(() => console.log("콜백함수 호출"))); // 콜백함수 호출
+console.log(callbackAdd(3, 4, (result) => console.log(result))); // 7
