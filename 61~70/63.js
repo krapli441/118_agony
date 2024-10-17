f786699a97edb2d14032bcb71f27f41a83427d92
krapli441 <tlfqj1208@gmail.com>
2024-10-17 15:11:18 +0900

    63번 문제 수정

--- 61~70/63.js
+++ 61~70/63.js
@@ -6,6 +6,12 @@ class Car {
   }
 }
 
+// Car 클래스의 인스턴스 생성
 const car = new Car();
 
-console.log(car instanceof Car);
+// car가 Car 클래스의 인스턴스인지 확인
+console.log(car instanceof Car); // 출력: true
+
+// class Car는 Car 클래스를 정의하고 constuctor()는 클래스 인스턴스를 생성할 때 호출된다.
+// const car = new Car(); 구문은 Car 클래스의 인스턴스를 생성하여 car 변수에 할당한다.
+// instanceof 연산자를 통해 인스턴스 여부를 확인한다.
