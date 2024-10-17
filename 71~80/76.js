df35ec246ba032b3141eb54e10531d7620ce3c95
krapli441 <tlfqj1208@gmail.com>
2024-10-17 17:51:58 +0900

    76

---
+++ 71~80/76.js
@@ -0,0 +1,9 @@
+// * Map객체를 사용해 키-값 쌍의 컬렉션을 생성하는 코드를 작성해보세요.
+
+const map = new Map();
+
+map.set("apple", 100);
+map.set("banana", 200);
+map.set("orange", 300);
+
+console.log(map); // Map { 'apple' => 100, 'banana' => 200, 'orange' => 300 }
