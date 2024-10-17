1d48d72dd461e6b7b43313aa4fdf873158596383
krapli441 <tlfqj1208@gmail.com>
2024-10-17 17:07:14 +0900

    64

--- 61~70/64.js
+++ 61~70/64.js
@@ -1,1 +1,18 @@
-//
+//64
+
+// * 모듈을 만들고 그것을 내보내는 코드를 작성해보세요.
+
+// 변수 내보내기
+export const myVariable = 42;
+
+// 함수 내보내기
+export function myFunction() {
+  console.log("This is my function");
+}
+
+// 클래스 내보내기
+export class MyClass {
+  constructor() {
+    console.log("MyClass instance created");
+  }
+}
