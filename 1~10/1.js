bc6a13fe0cacdf0638ce3766d5be874bc0b2b229
krapli441 <tlfqj1208@gmail.com>
2024-10-17 12:16:16 +0900

    문제 최신화

--- 1~10/1.js
+++ 1~10/1.js
@@ -1,3 +1,7 @@
 //* "Hello World!"를 콘솔에 출력하는 코드를 작성하시오.
 
 console.log("Hello World!");
+
+// 2024. 10. 17
+
+console.log("Hello World!");
--- 31~40/34.js
+++ 31~40/34.js
@@ -1,9 +1,13 @@
 // * continue문을 사용하여 짝수만 출력하는 코드를 작성해보세요.
+// 자바스크립트의 반복문 (for , while , do-while)에서 사용되며
+// 현재 반복을 건너뛰고 다음 반복을 바로 시작하게 만든다.
 
 function continuer() {
   for (let i = 1; i <= 10; i++) {
-    if (i % 2 !== 0) continue;
-    console.log(i);
+    if (i % 2 !== 0) {
+      continue; // i가 홀수이면 아래 코드 실행을 건너뛰고 다음 반복으로 이동
+    }
+    console.log(i); // 짝수만 출력
   }
 }
 
